5117f72007202d18321ebdda6a3d1fabe7448a24
nicholashornick <33236019+nicholashornick@users.noreply.github.com>
2019-03-07 15:02:04 -0500

    Made changes through random spawning of enemies

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static public Hero S;    //Singleton // a
+    [Header("Set in Inspector")]
+// These fields control the movement of the ship
+public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this; // Set the Singleton // a
 }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+      }
+    }
+    void Update()
+    {
+        // Pull in information from the Input class
+        float xAxis =
+        Input.GetAxis("Horizontal"); // b
+        float yAxis =
+        Input.GetAxis("Vertical"); // b
+                                   // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+        // Rotate the ship to make it feel more
+      //  dynamic // c
+transform.rotation =
+Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
+
---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    static public Main S; // A singleton for Main
+[Header("Set in Inspector")]
+public GameObject[] prefabEnemies; // Array of Enemy prefabs
+public float enemySpawnPerSecond = 0.5f; // # Enemies/second
+public float enemyDefaultPadding = 1.5f; // Padding forposition
+private BoundsCheck bndCheck;
+    void Awake()
+    {
+        S = this;
+        // Set bndCheck to reference the BoundsCheck component on this
+        bndCheck = GetComponent<BoundsCheck>();
+        // Invoke SpawnEnemy() once (in 2 seconds, based on defaultvalues)
+Invoke("SpawnEnemy", 1f / enemySpawnPerSecond
+); // a
+    }
+    public void SpawnEnemy()
+    {
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0,
+        prefabEnemies.Length); // b
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]
+        ); // c
+           // Position the Enemy above the screen with a random x position
+        float enemyPadding =
+        enemyDefaultPadding; // d
+        if (go.GetComponent<BoundsCheck>() != null)
+        { // e
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>
+            ().radius);
+        }
+        // Set the initial position for the spawned
+        //Enemy // f
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+        // Invoke SpawnEnemy() again
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond
+        ); // g
+    }
+}
